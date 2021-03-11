5d40750b6b11e3f20ad3a9288e5912a1c6509638
Krushang Kasundra <kruzkasu223@gmail.com>
2021-03-11 15:28:40 +0530

    normal setup done

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function App() {
     return (
         <div>
-            <h1>Hellow...</h1>
-            <h2>kruz</h2>
+            <h1>Ignite Gamer</h1>
         </div>
     );
 }
